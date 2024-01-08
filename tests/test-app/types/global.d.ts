b1bb883295fa4abd934435ac6d02978ca0ea8441
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-01-08 11:52:01 -0500

    Upgrade to ember-page-title@v8.2.0, which provides its own types now

--- tests/test-app/types/global.d.ts
+++ tests/test-app/types/global.d.ts
@@ -1,1 +1,9 @@
 import '@glint/environment-ember-loose';
+
+import type PageTitle from 'ember-page-title/template-registry';
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry extends PageTitle {
+    /* your local loose-mode entries here */
+  }
+}
