83b111b6cfa3cc68468e0ec01ed3a44688284591
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-01-08 16:32:10 -0500

    Copy modifier and service from ember-resources

--- reactiveweb/src/-private/types.ts
+++ reactiveweb/src/-private/types.ts
@@ -13,3 +13,43 @@ export type Stage1Decorator = (
 export interface Class<Instance> {
   new (...args: unknown[]): Instance;
 }
+
+// This is a bit of a lie, because we don't have an easy way to
+// represent an object with no properties.
+export type EmptyObject = {};
+
+export type GetOrElse<Obj, K, Fallback> = K extends keyof Obj ? Obj[K] : Fallback;
+
+export type ArgsFor<S> =
+  // Signature['Args']
+  S extends { Named?: object; Positional?: unknown[] }
+    ? {
+        Named: GetOrElse<S, 'Named', EmptyObject>;
+        Positional: GetOrElse<S, 'Positional', []>;
+      }
+    : S extends { named?: object; positional?: unknown[] }
+    ? {
+        Named: GetOrElse<S, 'named', EmptyObject>;
+        Positional: GetOrElse<S, 'positional', []>;
+      }
+    : { Named: EmptyObject; Positional: [] };
+
+export type ElementFor<S> = 'Element' extends keyof S
+  ? S['Element'] extends Element
+    ? S['Element']
+    : Element
+  : Element;
+
+/**
+ * Converts a variety of types to the expanded arguments type
+ * that aligns with the 'Args' portion of the 'Signature' types
+ * from ember's helpers, modifiers, components, etc
+ */
+export type ExpandArgs<T> = T extends any[]
+  ? ArgsFor<{ Positional: T }>
+  : T extends any
+  ? ArgsFor<T>
+  : never;
+
+export type Positional<T> = ExpandArgs<T>['Positional'];
+export type Named<T> = ExpandArgs<T>['Named'];
---
+++ reactiveweb/src/resource/modifier/manager.ts
@@ -0,0 +1,88 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { getValue } from '@glimmer/tracking/primitives/cache';
+import { destroy } from '@ember/destroyable';
+import { invokeHelper } from '@ember/helper';
+import { capabilities } from '@ember/modifier';
+
+interface ArgsWrapper {
+  positional?: readonly unknown[];
+  named?: Record<string, any>;
+}
+
+import type { FunctionBasedModifierDefinition } from './index.ts';
+import type { ElementFor } from '#types';
+
+interface State<S> {
+  instance: FunctionBasedModifierDefinition<S>;
+  helper: ReturnType<typeof invokeHelper> | null;
+}
+
+interface CreatedState<S> extends State<S> {
+  element: null;
+}
+
+interface InstalledState<S> extends State<S> {
+  element: ElementFor<S>;
+}
+
+// Wraps the unsafe (b/c it mutates, rather than creating new state) code that
+// TS does not yet understand.
+function installElement<S>(state: CreatedState<S>, element: ElementFor<S>): InstalledState<S> {
+  // SAFETY: this cast represents how we are actually handling the state machine
+  // transition: from this point forward in the lifecycle of the modifier, it
+  // always behaves as `InstalledState<S>`. It is safe because, and *only*
+  // because, we immediately initialize `element`. (We cannot create a new state
+  // from the old one because the modifier manager API expects mutation of a
+  // single state bucket rather than updating it at hook calls.)
+  const installedState = state as State<S> as InstalledState<S>;
+
+  installedState.element = element;
+
+  return installedState;
+}
+
+function arrangeArgs(element: Element, args: any) {
+  const { positional, named } = args;
+
+  let flattenedArgs = [element, ...positional];
+
+  if (Object.keys(named).length > 0) {
+    flattenedArgs.push(named);
+  }
+
+  return flattenedArgs;
+}
+
+export default class FunctionBasedModifierManager<S> {
+  capabilities = capabilities('3.22');
+
+  createModifier(instance: FunctionBasedModifierDefinition<S>): CreatedState<S> {
+    return { element: null, instance, helper: null };
+  }
+
+  installModifier(createdState: CreatedState<S>, element: ElementFor<S>, args: ArgsWrapper): void {
+    const state = installElement(createdState, element);
+
+    this.updateModifier(state, args);
+  }
+
+  updateModifier(state: InstalledState<S>, args: ArgsWrapper): void {
+    if (state.helper) {
+      destroy(state.helper);
+    }
+
+    state.helper = invokeHelper(this, state.instance, () => {
+      let foo = arrangeArgs(state.element, args);
+
+      return { positional: foo };
+    });
+
+    getValue(state.helper);
+  }
+
+  destroyModifier(state: InstalledState<S>): void {
+    if (state.helper) {
+      destroy(state.helper);
+    }
+  }
+}
