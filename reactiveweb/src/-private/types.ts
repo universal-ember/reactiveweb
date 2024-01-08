4b092e57b5a707a83dff42a25a0fa9f714c4825e
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-01-08 14:20:09 -0500

    Add `@link` (#36)
    * Add @link
    
    * lint:fix

---
+++ reactiveweb/src/-private/types.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export interface Stage1DecoratorDescriptor {
+  initializer: () => unknown;
+}
+
+export type Stage1Decorator = (
+  prototype: object,
+  key: string | symbol,
+  descriptor?: Stage1DecoratorDescriptor
+) => any;
+
+export interface Class<Instance> {
+  new (...args: unknown[]): Instance;
+}
