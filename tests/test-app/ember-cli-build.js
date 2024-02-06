a9454d978a8f79ab9967a6933c6086fde3de897c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-02-06 00:25:12 -0800

    Be sure to use the babel plugin

--- tests/test-app/ember-cli-build.js
+++ tests/test-app/ember-cli-build.js
@@ -3,8 +3,22 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
+  let babel = {};
+
+  if (process.env.EMBER_TRY_CURRENT_SCENARIO === 'ember-concurrency-4.0') {
+    babel = {
+      plugins: [
+        // ... any other plugins
+        require.resolve('ember-concurrency/async-arrow-task-transform'),
+
+        // NOTE: put any code coverage plugins last, after the transform.
+      ],
+    };
+  }
+
   let app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
+    babel,
     autoImport: {
       watchDependencies: ['reactiveweb'],
     },
